1287f56f1ff9f3f84f5051f144c63dca7f643398
Roy Smart <roytsmart@gmail.com>
2022-09-13 20:34:32 -0600

    Added `named_arrays.tests.test_core.test_shape_broacasted()`.

--- named_arrays/tests/test_core.py
+++ named_arrays/tests/test_core.py
@@ -14,3 +14,19 @@ def test_broadcast_shapes():
     assert shape_broadcasted == dict(x=num_x, y=num_y, z=num_z)
 
 
+def test_shape_broadcasted():
+
+    num_x = 5
+    num_y = 7
+    num_z = 11
+
+    shape_1 = dict(x=num_x)
+    shape_2 = dict(x=num_x, y=num_y)
+    shape_3 = dict(y=num_y, z=num_z)
+
+    array_1 = na.ScalarArray.empty(shape_1)
+    array_2 = na.ScalarArray.empty(shape_2)
+    array_3 = na.ScalarArray.empty(shape_3)
+
+    shape_broadcasted = na.shape_broadcasted(array_1, array_2, array_3)
+    assert shape_broadcasted == dict(x=num_x, y=num_y, z=num_z)
