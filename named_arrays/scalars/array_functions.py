7e33c61832b861dc28a8b6bb5267c14675fcaf14
Roy Smart <roytsmart@gmail.com>
2022-11-24 23:07:59 -0700

    Continued improvement to `__array_function__` handling.
    Signed-off-by: Roy Smart <roytsmart@gmail.com>

---
+++ named_arrays/scalars/array_functions.py
@@ -0,0 +1,141 @@
+from typing import Sequence, Callable, Type
+import numpy as np
+import astropy.units as u
+
+from . import core as scalars
+
+__all__ = [
+    'HANDLED_FUNCTIONS'
+]
+
+DEFAULT_FUNCTIONS = [
+    np.ndim,
+    np.argmin,
+    np.nanargmin,
+    np.min,
+    np.nanmin,
+    np.argmax,
+    np.nanargmax,
+    np.max,
+    np.nanmax,
+    np.sum,
+    np.nansum,
+    np.mean,
+    np.nanmean,
+    np.std,
+    np.nanstd,
+    np.var,
+    np.nanvar,
+    np.median,
+    np.nanmedian,
+    np.percentile,
+    np.nanpercentile,
+    np.quantile,
+    np.nanquantile,
+    np.all,
+    np.any,
+    np.ptp,
+    np.count_nonzero,
+]
+HANDLED_FUNCTIONS = dict()
+
+
+def _axis_normalized(
+        a: scalars.AbstractScalarArray,
+        axis: None | str | Sequence[str],
+) -> tuple[str]:
+    if axis is None:
+        result = a.axes
+    elif isinstance(axis, str):
+        result = axis,
+    else:
+        result = tuple(axis)
+    return result
+
+
+def _calc_axes_new(
+        a: scalars.AbstractScalarArray,
+        axis: None | str | Sequence[str],
+        *,
+        keepdims: None | bool = None,
+) -> tuple[str, ...]:
+    if keepdims is None:
+        keepdims = False
+    if keepdims:
+        return a.axes
+    else:
+        axis = _axis_normalized(a, axis)
+        return tuple(ax for ax in a.axes if ax not in axis)
+
+
+def array_function_default(
+        func: Callable,
+        a: scalars.AbstractScalarArray,
+        axis: None | str | Sequence[str] = None,
+        dtype: None | Type = None,
+        out: None | scalars.ScalarArray = None,
+        keepdims: None | bool = None,
+        initial: None | bool | int | float | complex | u.Quantity = None,
+        where: None | scalars.AbstractScalarArray = None,
+):
+    kwargs = dict()
+
+    if axis is not None:
+        kwargs['axis'] = tuple(a.axes.index(ax) for ax in _axis_normalized(a, axis=axis))
+    if dtype is not None:
+        kwargs['dtype'] = dtype
+    if out is not None:
+        kwargs['out'] = out
+    if keepdims is not None:
+        kwargs['keepdims'] = keepdims
+    if initial is not None:
+        kwargs['initial'] = initial
+    if where is not None:
+        kwargs['where'] = where
+
+    return scalars.ScalarArray(
+        ndarray=func(a.ndarray, **kwargs),
+        axes=_calc_axes_new(a, axis=axis, keepdims=keepdims),
+    )
+
+
+def implements(numpy_function: Callable):
+    """Register an __array_function__ implementation for AbstractScalarArray objects."""
+    def decorator(func):
+        HANDLED_FUNCTIONS[numpy_function] = func
+        return func
+    return decorator
+
+
+@implements(np.broadcast_to)
+def broadcast_to(
+        array: scalars.AbstractScalarArray,
+        shape: dict[str, int],
+):
+    return scalars.ScalarArray(
+        ndarray=np.broadcast_to(array.ndarray_aligned(shape), tuple(shape.values()), subok=True),
+        axes=tuple(shape.keys()),
+    )
+
+
+@implements(np.shape)
+def shape(
+        a: scalars.AbstractScalarArray,
+) -> dict[str, int]:
+    return a.shape
+
+
+@implements(np.transpose)
+def transpose(
+        a: scalars.AbstractScalarArray,
+        axes: None | Sequence[str] = None,
+):
+    axes = tuple(reversed(a.axes)) if axes is None else axes
+    return scalars.ScalarArray(
+        ndarray=np.transpose(
+            a=a.ndarray,
+            axes=tuple(a.axes.index(ax) for ax in axes),
+        ),
+        axes=axes,
+    )
+
--- named_arrays/scalars/core.py
+++ named_arrays/scalars/core.py
@@ -38,8 +38,6 @@ StartExponentT = TypeVar('StartExponentT', bound='ScalarLike')
 StopExponentT = TypeVar('StopExponentT', bound='ScalarLike')
 BaseT = TypeVar('BaseT', bound='ScalarLike')
 
-HANDLED_FUNCTIONS = {}
-
 
 @dataclasses.dataclass(eq=False)
 class AbstractScalar(
@@ -391,22 +389,28 @@ class AbstractScalarArray(
 
         return NotImplemented
 
-    def __array_function__replacement(
+    def __array_function__(
             self: Self,
             func: Callable,
             types: Collection,
             args: tuple,
             kwargs: dict[str, Any],
     ):
-        if func not in HANDLED_FUNCTIONS:
-            return NotImplemented
-        # Note: this allows subclasses that don't override
-        # __array_function__ to handle MyArray objects
+        from . import array_functions
+
         if not all(issubclass(t, na.AbstractArray) for t in types):
             return NotImplemented
-        return HANDLED_FUNCTIONS[func](*args, **kwargs)
 
-    def __array_function__(
+        if func in array_functions.DEFAULT_FUNCTIONS:
+            return array_functions.array_function_default(func, *args, **kwargs)
+
+        if func in array_functions.HANDLED_FUNCTIONS:
+            return array_functions.HANDLED_FUNCTIONS[func](*args, **kwargs)
+
+        return NotImplemented
+
+
+    def __array_function__old(
             self: Self,
             func: Callable,
             types: Collection,
@@ -862,56 +866,6 @@ class AbstractScalarArray(
             result = result << self.unit
 
         return result
-
-
-def implements(numpy_function: Callable):
-    """Register an __array_function__ implementation for AbstractScalarArray objects."""
-    def decorator(func):
-        HANDLED_FUNCTIONS[numpy_function] = func
-        return func
-    return decorator
-
-
-def _axis_normalized(
-        a: AbstractScalarArray,
-        axis: None | str | Sequence[str],
-) -> tuple[str]:
-    if axis is None:
-        result = a.axes
-    elif isinstance(axis, str):
-        result = axis,
-    else:
-        result = tuple(axis)
-    return result
-
-
-def _calc_axes_new(
-        a: AbstractScalarArray,
-        axis: None | str | Sequence[str],
-        *,
-        keepdims: bool = False,
-) -> tuple[str, ...]:
-    if keepdims:
-        return a.axes
-    else:
-        axis = _axis_normalized(a, axis)
-        return tuple(ax for ax in a.axes if ax not in axis)
-
-
-def count_nonzero(
-        a: AbstractScalarArray,
-        axis: None | str | Sequence[str] = None,
-        *,
-        keepdims: bool = False,
-) -> ScalarArray:
-    return ScalarArray(
-        ndarray=np.count_nonzero(
-            a=a.ndarray,
-            axis=tuple(a.axes.index(ax) for ax in _axis_normalized(a, axis=axis)),
-            keepdims=keepdims,
-        ),
-        axes=_calc_axes_new(a, axis=axis, keepdims=keepdims)
-    )
 
 
 ScalarLike = Union[na.QuantityLike, AbstractScalar]
