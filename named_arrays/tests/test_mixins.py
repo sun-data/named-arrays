1c18f8b20ed85e4a387fbac5d3f12f25e0dc1eb7
Roy Smart <roytsmart@gmail.com>
2022-12-01 09:41:42 -0700

    Added `typing.Self` to methods in `named_array.tests.test_mixin` module.
    Signed-off-by: Roy Smart <roytsmart@gmail.com>

--- named_arrays/tests/test_mixins.py
+++ named_arrays/tests/test_mixins.py
@@ -1,3 +1,4 @@
+from typing import Self
 import abc
 import dataclasses
 import numpy as np
@@ -8,14 +9,14 @@ class AbstractTestCopyable(
     abc.ABC
 ):
 
-    def test_copy_shallow(self, array: named_arrays.mixins.CopyableMixin):
+    def test_copy_shallow(self: Self, array: named_arrays.mixins.CopyableMixin):
         array_copy = array.copy_shallow()
         assert isinstance(array_copy, named_arrays.mixins.CopyableMixin)
         assert dataclasses.is_dataclass(array_copy)
         for field in dataclasses.fields(array_copy):
             assert getattr(array, field.name) is getattr(array_copy, field.name)
 
-    def test_copy(self, array: named_arrays.mixins.CopyableMixin):
+    def test_copy(self: Self, array: named_arrays.mixins.CopyableMixin):
         array_copy = array.copy()
         assert isinstance(array_copy, named_arrays.mixins.CopyableMixin)
         assert dataclasses.is_dataclass(array_copy)
