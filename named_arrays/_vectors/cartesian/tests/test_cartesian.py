96aefcaaa0f0a501a8291ba5efc3e78dee9bb1b7
Roy Smart <roytsmart@gmail.com>
2023-03-05 14:10:29 -0700

    Improvements to `named_arrays._vectors.cartesian.tests.test_cartesian.AbstractTestAbstractCartesianVector.TestUfuncBinary`.

--- named_arrays/_vectors/cartesian/tests/test_cartesian.py
+++ named_arrays/_vectors/cartesian/tests/test_cartesian.py
@@ -104,65 +104,75 @@ class AbstractTestAbstractCartesianVectorArray(
                 ufunc: np.ufunc,
                 array: None | bool | int | float | complex | str | na.AbstractArray | na.AbstractCartesianVectorArray,
                 array_2: None | bool | int | float | complex | str | na.AbstractArray | na.AbstractCartesianVectorArray,
-                # out: bool,
         ):
-            out = False
-
             super().test_ufunc_binary(ufunc, array, array_2)
 
-            if isinstance(array, na.AbstractCartesianVectorArray):
-                type_array = array.type_array
+            if isinstance(array, na.AbstractVectorArray):
                 array_normalized = array
-                if isinstance(array_2, na.AbstractCartesianVectorArray):
+                if isinstance(array_2, na.AbstractVectorArray):
                     array_2_normalized = array_2
                 else:
-                    array_2_normalized = type_array.from_scalar(array_2)
-
+                    array_2_normalized = array.type_array.from_scalar(array_2)
             else:
-                if isinstance(array_2, na.AbstractCartesianVectorArray):
-                    type_array = array_2.type_array
+                if isinstance(array_2, na.AbstractVectorArray):
+                    array_normalized = array_2.type_array.from_scalar(array)
                     array_2_normalized = array_2
-                    array_normalized = type_array.from_scalar(array)
+                else:
+                    raise ValueError("One of the test arrays should be vectors")
 
             components = array_normalized.components
             components_2 = array_2_normalized.components
 
-            components_expected = tuple(dict() for _ in range(ufunc.nout))
+            kwargs = dict()
+            kwargs_components = {c: dict() for c in components}
+
+            try:
+                if ufunc in [np.power, np.float_power]:
+                    kwargs["where"] = (array_2 >= 1) & (array >= 0)
+                elif ufunc in [np.divide, np.floor_divide, np.remainder, np.fmod, np.divmod]:
+                    kwargs["where"] = array_2 != 0
+            except u.UnitConversionError:
+                pass
+
+            for c in components:
+                for k in kwargs:
+                    if isinstance(kwargs[k], na.AbstractVectorArray):
+                        kwargs_components[c][k] = kwargs[k].components[c]
+                    else:
+                        kwargs_components[c][k] = kwargs[k]
+
+
+            result_expected = tuple(array_normalized.type_array() for _ in range(ufunc.nout))
             try:
                 for c in components:
-                    result_c = ufunc(components[c], components_2[c])
+                    result_c = ufunc(components[c], components_2[c], **kwargs_components[c])
                     if ufunc.nout == 1:
                         result_c = (result_c,)
                     for i in range(ufunc.nout):
-                        components_expected[i][c] = result_c[i]
+                        result_expected[i].components[c] = result_c[i]
             except Exception as e:
                 with pytest.raises(type(e)):
-                    ufunc(array, array_2)
+                    ufunc(array, array_2, **kwargs)
                 return
 
-            result_expected = tuple(type_array.from_components(components_expected[i]) for i in range(ufunc.nout))
+            result = ufunc(array, array_2, **kwargs)
 
-            if out:
-                out = tuple(0 * result_expected[i] for i in range(ufunc.nout))
-                # for i in range(ufunc.nout):
-                #     for c in components:
-                #         if not isinstance(out[i].components[c], na.AbstractArray):
-                #             out[i].components[c] = np.asanyarray(out[i].components[c])
-                #         elif isinstance(out[i].components[c], na.AbstractScalarArray):
-                #             out[i].components[c].ndarray = np.asanyarray(out[i].components[c].ndarray)
-
-            else:
-                out = tuple(None for _ in range(ufunc.nout))
-
-            result = ufunc(array, array_2, out=out[0] if ufunc.nout == 1 else out)
             if ufunc.nout == 1:
-                result = (result, )
+                out = 0 * result
+            else:
+                out = tuple(0 * r for r in result)
+
+            result_out = ufunc(array, array_2, out=out, **kwargs)
+
+            if ufunc.nout == 1:
+                out = (out,)
+                result = (result,)
+                result_out = (result_out,)
 
             for i in range(ufunc.nout):
-                assert np.all(result[i] == result_expected[i])
-
-                if out[i] is not None:
-                    assert result[i] is out[i]
+                assert np.all(result[i] == result_expected[i], **kwargs)
+                assert np.all(result[i] == result_out[i], **kwargs)
+                assert result_out[i] is out[i]
 
 
 class AbstractTestAbstractExplicitCartesianVectorArray(
