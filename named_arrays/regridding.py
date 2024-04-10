77e03464e772f52200d451016d394ccbd76d8571
jacobdparker <jacobdparker@gmail.com>
2024-04-10 10:42:46 -0500

    Adding broadcasting at the named_array level that was previously inside regridding package.  Cleaning up print statements.

--- named_arrays/regridding.py
+++ named_arrays/regridding.py
@@ -48,55 +48,54 @@ def weights(
         axis_output: None | Sequence[str] = None,
         method: Literal['multilinear', 'conservative'] = 'multilinear',
 ) -> na.AbstractArray:
-    if coordinates_output.ndim == 1:
-        method = 'multilinear'
-    else:
-        method = 'conservative'
 
     if axis_output is not None:
         return NotImplementedError
 
-    # check old and new coordinates are the same type AbstractArray.  May want to compare compenents of
-    # cartesian_nd instead
     if np.any(coordinates_output.cartesian_nd.components.keys == coordinates_input.cartesian_nd.components.keys):
         return ValueError
 
     input_coordinates_broadcasted = coordinates_input.cartesian_nd.broadcasted
     output_coordinates_broadcasted = coordinates_output.cartesian_nd.broadcasted
 
-    #broadcast input coordinates against new output if missing axes
-    broadcasted_input_shape = {}
+    # broadcast input(old) coordinates against output(new) coordinates along missing axes
+    broadcasted_input_shape = input_coordinates_broadcasted.shape
     for key in output_coordinates_broadcasted.shape:
-        if key in input_coordinates_broadcasted.shape:
-            broadcasted_input_shape[key] = input_coordinates_broadcasted.shape[key]
-        else:
+        if key not in input_coordinates_broadcasted.shape:
             broadcasted_input_shape[key] = output_coordinates_broadcasted.shape[key]
 
     input_coordinates_broadcasted = input_coordinates_broadcasted.broadcast_to(broadcasted_input_shape)
 
-    output_broadcasted_shape = output_coordinates_broadcasted.shape
-    print(f'{output_broadcasted_shape=}')
-    print(f'{input_coordinates_broadcasted.shape=}')
+    # broadcast output(new) coordinates against input(old) coordinates along missing axes
+    broadcasted_output_shape = output_coordinates_broadcasted.shape
+    for key in input_coordinates_broadcasted.shape:
+        if key not in output_coordinates_broadcasted.shape:
+            broadcasted_output_shape[key] = input_coordinates_broadcasted.shape[key]
+
+    output_coordinates_broadcasted = output_coordinates_broadcasted.broadcast_to(broadcasted_output_shape)
+
+    orthogonal_axes = [key for key in input_coordinates_broadcasted.shape if key not in axis_input]
 
     coords_input = []
     for c in input_coordinates_broadcasted.components:
         component = input_coordinates_broadcasted.components[c]
         coords_input.append(
-            component.broadcast_to(dict([(c, component.shape[c]) for c in output_broadcasted_shape])).ndarray
+            component.broadcast_to(dict([(c, component.shape[c]) for c in broadcasted_output_shape])).ndarray
         )
 
     coords_output = []
     for c in output_coordinates_broadcasted.components:
         component = output_coordinates_broadcasted.components[c]
         coords_output.append(
-            component.broadcast_to(dict([(c, component.shape[c]) for c in output_broadcasted_shape])).ndarray
+            component.broadcast_to(dict([(c, component.shape[c]) for c in broadcasted_output_shape])).ndarray
         )
 
-    interp_axes = tuple(list(output_broadcasted_shape.keys()).index(key) for key in axis_input)
-    # print(f'{coords_input.shape=}')
-    # print(f'{coords_output.shape=}')
-    print(f'{axis_input=}')
-    print(f'{interp_axes=}')
+    interp_axes = tuple(list(broadcasted_output_shape.keys()).index(key) for key in axis_input)
+
+    if len(coords_input) == 1:
+        method = 'multilinear'
+    elif len(coords_input) == 2:
+        method = 'conservative'
 
     weights, shape_input, shape_output = regridding.weights(
         coordinates_input=tuple(coords_input),
@@ -106,20 +105,15 @@ def weights(
         method=method,
     )
 
-    shape_input = dict([(key, shape) for key, shape in zip(axis_input, shape_input)])
-    shape_output = dict([(key, shape) for key, shape in zip(axis_input, shape_output)])
-
-    # values_input.broadcast_to(dict([(c, values_input.shape[c]) for c in new_shape]))
-    print(f'{weights=}')
-    print(f'{weights.shape=}')
-    print(f'{shape_input=}')
-    print(f'{shape_output=}')
+    shape_input = dict([(key, shape) for key, shape in zip(broadcasted_output_shape.keys(), shape_input)])
+    shape_output = dict([(key, shape) for key, shape in zip(broadcasted_output_shape.keys(), shape_output)])
+    weights = na.ScalarArray(np.array(weights), axes=orthogonal_axes)
 
     return weights, shape_input, shape_output
 
 
 def regrid_from_weights(
-        weights: np.ndarray,
+        weights: np.AbstractScalarArray,
         shape_input: na.AbstractArray,
         shape_output: na.AbstractArray,
         values_input: na.AbstractScalarArray,
@@ -127,21 +121,39 @@ def regrid_from_weights(
         axis_input: None | Sequence[str] = None,
         axis_output: None | Sequence[str] = None,
 ):
+    if axis_output is None:
+        axis_output = axis_input
+
+
+    #broadcast values along any new input coordinate dimensions
+    values_input_broadcasted_shape = na.broadcast_shapes(values_input.shape, shape_input)
+    values_input = values_input.broadcast_to(values_input_broadcasted_shape)
+    shape_input = values_input_broadcasted_shape
+
+    shape_orthogonal = {}
+    for key in shape_input:
+        if key not in axis_input:
+            shape_orthogonal[key] = shape_input[key]
+
+    weights = weights.broadcast_to(shape_orthogonal)
+
+    for key in shape_input:
+        if key not in shape_output:
+            shape_output[key] = shape_input[key]
+
     values_input_ndarray = values_input.ndarray
-    interp_axes = tuple(list(values_input.shape.keys()).index(key) for key in axis_input)
-    print(f'{interp_axes=}')
+    interp_axes_input = tuple(list(shape_input).index(key) for key in axis_input)
 
     values_output_ndarray = regridding.regrid_from_weights(
-        weights=weights,
+        weights=weights.ndarray,
         shape_input=tuple([shape_input[key] for key in shape_input]),
         shape_output=tuple([shape_output[key] for key in shape_input]),
         # every ndarray should be in the axes order of shape_input
         values_input=values_input_ndarray,
         # values_output=values_output,
-        axis_input=interp_axes,
-        axis_output=interp_axes,
+        axis_input=interp_axes_input,
+        axis_output=interp_axes_input,
     )
     values_output = na.ScalarArray(values_output_ndarray, axes=values_input.shape.keys())
-    print(f'{values_output.shape=}')
 
     return values_output
