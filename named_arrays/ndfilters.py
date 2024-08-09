fcc13fd2abd7bed0cf14a28680623ff89a99548b
Roy Smart <roytsmart@gmail.com>
2024-08-09 06:08:32 -0600

    Added `named_arrays.ndfilter.trimmed_mean_filter()` function.

--- named_arrays/ndfilters.py
+++ named_arrays/ndfilters.py
@@ -73,3 +73,71 @@ def mean_filter(
         size=size,
         where=where,
     )
+
+
+def trimmed_mean_filter(
+    array: ArrayT,
+    size: dict[str, int],
+    where: WhereT = True,
+) -> ArrayT | WhereT:
+    """
+    A thin wrapper around :func:`ndfilters.trimmed_mean_filter` for named arrays.
+
+    Parameters
+    ----------
+    array
+        The input array to be filtered.
+    size
+        The shape of the kernel over which the mean will be calculated.
+    where
+        A boolean mask used to select which elements of the input array are to
+        be filtered.
+
+    Examples
+    --------
+
+    Filter a sample image.
+
+    .. jupyter-execute::
+        :stderr:
+
+        import matplotlib.pyplot as plt
+        import scipy.datasets
+        import named_arrays as na
+
+        img = na.ScalarArray(scipy.datasets.ascent(), axes=("x", "y"))
+
+        img_filtered = na.ndfilters.trimmed_mean_filter(
+            array=img,
+            size=dict(x=21, y=21).
+        )
+
+        fig, axs = plt.subplots(
+            ncols=2,
+            sharex=True,
+            sharey=True,
+            constrained_layout=True,
+        )
+        axs[0].set_title("original image");
+        na.plt.imshow(
+            X=img,
+            axis_x="x",
+            axis_y="y",
+            ax=axs[0],
+            cmap="gray",
+        );
+        axs[1].set_title("mean filtered image");
+        na.plt.imshow(
+            X=img_filtered,
+            axis_x="x",
+            axis_y="y",
+            ax=axs[1],
+            cmap="gray",
+        );
+    """
+    return na._named_array_function(
+        func=trimmed_mean_filter,
+        array=array,
+        size=size,
+        where=where,
+    )
