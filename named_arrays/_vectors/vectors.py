0f86eaf629d11bb19ee33a1b1c6f0147c346d61f
jacobdparker <jacobdparker@gmail.com>
2025-03-21 15:17:27 -0400

    Added type hinting to nominal

--- named_arrays/_vectors/vectors.py
+++ named_arrays/_vectors/vectors.py
@@ -145,7 +145,7 @@ class AbstractVectorArray(
         return self.type_explicit.from_components(dict.fromkeys(self.components, 0))
 
     @property
-    def nominal(self):
+    def nominal(self) -> na.AbstractExplicitVectorArray:
         components = self.components
         components = {c: components[c].nominal if isinstance(components[c], na.AbstractUncertainScalarArray) else components[c] for c in components}
         return self.type_explicit.from_components(components)
